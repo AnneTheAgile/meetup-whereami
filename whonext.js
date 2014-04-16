1c3fb9afec20cb20356d35b7f3590afb518d1194
AnneTheAgile <cqcinfo@yahoo.com>
2014-04-16 19:02:51 -0400

    Added whonext.js: tell the username of the next person who would get to come off the waitlist.

---
+++ whonext.js
@@ -0,0 +1,14 @@
+(function(window) {
+  // was var $waitlistEl = $('#rsvp-list-waitlist');
+  var doc = document, obj = { meta: [], taxonomy: [] };
+  var item = document.querySelector('#rsvp-list-waitlist li:first-child'),
+    subItem = item.querySelector('.figureset-description h5 a');
+ 
+  console.log(subItem.text); // "Sub-menu Item"
+
+  if (subItem.text != "")
+    alert(subItem.text + " is the next person who will get to go from the waitlist");
+  else 
+    alert("Error? Nothing in ; "+subItem.text)
+
+})(window);
